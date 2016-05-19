d60df5105a8f9579712173e571ab58667c3f0751
Roman Dubinin <romanonthego@gmail.com>
2016-05-19 15:43:23 +0600

    unstore-all-files script added
    to purge project from files alltogether

--- src/unstore-all-files.js
+++ src/unstore-all-files.js
@@ -2,39 +2,60 @@
 import request from 'request'
 import colors from 'colors'
 
-function iterateThroughPage(url, publicKey, privateKey) {
+function iterateThroughPage(url, publicKey, privateKey, deleteNotUnstore) {
   const headers = {
     Authorization: `Uploadcare.Simple ${publicKey}:${privateKey}`
   }
 
   request.get({
-    url: url,
-    headers: headers
+    url,
+    headers
   }, (err, res, body) => {
-    body.results.map((file) => {
-      request.del({
-        url: `https://api.uploadcare.com/files/${file.uuid}/storage`,
-        headers: headers
-      }, (delErr) => {
-        if (delErr) {
-          console.log(`ERROR deleting ${file.uuid.underline}, ${JSON.stringify(delErr)}`.red)
-        } else {
-          console.log(`${file.uuid.underline} were deleted`.green)
-        }
-      })
-    })
+    try {
+      const {
+        results,
+        next,
+      } = JSON.parse(body)
 
-    if (body.next) {
-      iterateThroughPage(body.next, publicKey, privateKey)
+      results.map((file) => {
+        const fileUnstoreUrl = `https://api.uploadcare.com/files/${file.uuid}/${deleteNotUnstore ? '' : 'storage/'}`
+        request.del({
+          url: `https://api.uploadcare.com/files/${file.uuid}/`,
+          headers
+        }, (delErr) => {
+          if (delErr) {
+            console.log(`ERROR deleting ${file.uuid.underline}: ${JSON.stringify(delErr)}`.red)
+          } else {
+            console.log(`${file.uuid.underline} were ${deleteNotUnstore ? 'deleted' : 'unstored'}`.green)
+          }
+        })
+      })
+
+      if (next) {
+        console.log('Goingin to next page'.underline.green)
+
+        iterateThroughPage(next, publicKey, privateKey, deleteNotUnstore)
+      }
+
+    } catch (e) {
+      console.log('Error deleting:'.red, e.stack)
     }
   })
 }
 
-export default function unstoreAllFiles(publicKey, privateKey) {
+
+export default function unstoreAllFiles(publicKey = '', privateKey = '', options = {}) {
   if (!publicKey.length || !privateKey.length) {
     console.log('no private/pubic key were provided, skipping deleting files')
     return
   }
 
-  iterateThroughPage(`https://api.uploadcare.com/files/?stored=true&limit=1000`, publicKey, privateKey)
+  const {
+    deleteNotUnstore = true,
+    limit = 100,
+  } = options
+
+  const url = `https://api.uploadcare.com/files/?stored=true&limit=${limit}`
+
+  iterateThroughPage(url, publicKey, privateKey, deleteNotUnstore)
 }
